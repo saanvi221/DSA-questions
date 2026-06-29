c342cb7f0ce5940a7a5662abae548a06cc14f8e2
Saanvi Mishra <saanvi221@outlook.com>
2026-06-29 16:57:01 +0530

    Time: 20 ms (30.90%) | Memory: 98.1 MB (27.93%) - LeetSync

---
+++ 77-combinations/combinations.java
@@ -0,0 +1,21 @@
+class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        backtrack(1, n, k, new ArrayList<>());
+        return result;
+    }
+
+    private void backtrack(int start, int n, int k, List<Integer> temp) {
+        if (temp.size() == k) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            temp.add(i);
+            backtrack(i + 1, n, k, temp);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
