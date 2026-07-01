25f98e1bded9d7554fffdb888c8bd82e2973c568
Saanvi Mishra <saanvi221@outlook.com>
2026-07-01 12:47:28 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (33.09%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public boolean isValidBST(TreeNode root) {
+        return validate(root, null, null);
+    }
+
+    private boolean validate(TreeNode node, Integer low, Integer high) {
+        if (node == null)
+            return true;
+
+        if (low != null && node.val <= low)
+            return false;
+
+        if (high != null && node.val >= high)
+            return false;
+
+        return validate(node.left, low, node.val) &&
+               validate(node.right, node.val, high);
+    }
+}
